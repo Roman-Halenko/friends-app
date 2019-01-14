ec3ed9afa8aa90d3706398d34146cc100c0e2c8c
Roman Halenko <owlbro.popping@gmail.com>
2019-01-14 03:45:02 +0200

    Add sorting functionality

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const sortBtn = document.getElementById('sort_btn');
 
 const header = document.querySelector('header');
 
-let searchField = document.getElementById('search');
+const searchField = document.getElementById('search');
+
+const select = document.getElementById('input-sort');
 
 fetch('https://randomuser.me/api/?results=60&nat=us')
 .then(response => response.json())
@@ -20,11 +22,63 @@ function controller(srcArr) {
     list = srcArr.filter(e => e.name.last.includes(inputStr) || e.name.first.includes(inputStr));
     renderHTML(list);
   }
-  function sortByAge(e) {
-    list = srcArr.sort();
+
+  function sortByAgeDesc(a, b) {
+    list.sort((a, b) => b.dob.age - a.dob.age);
     renderHTML(list);
   }
-  searchField.addEventListener('keyup', searchByName);
+
+  function sortByAgeAsc(a, b) {
+    list.sort((a, b) =>  a.dob.age - b.dob.age);
+    renderHTML(list);
+  }
+
+  function sortByNameAz() {
+    list.sort((a, b) => {
+      let nameA = a.name.first;
+      let nameB = b.name.first;
+      if (nameA > nameB) {
+        return 1;
+      }
+      if (nameA < nameB) {
+        return -1;
+      }
+      return 0;
+    })
+    renderHTML(list);
+  }
+
+  function sortByNameZa() {
+    list.sort((a, b) => {
+      let nameA = a.name.first;
+      let nameB = b.name.first;
+      if (nameA < nameB) {
+        return 1;
+      }
+      if (nameA > nameB) {
+        return -1;
+      }
+      return 0;
+    })
+    renderHTML(list);
+  }
+
+  searchField.addEventListener('input', searchByName);
+
+  select.addEventListener('change', e => {
+    if (e.target.value === 'ASC') {
+      sortByAgeAsc();
+    } else if (e.target.value === 'DESC') {
+      sortByAgeDesc();
+    } else if (e.target.value === 'ZA') {
+      console.log('Works ZA');
+      sortByNameZa();
+    } else if (e.target.value === 'AZ') {
+      console.log('Works AZ');
+      sortByNameAz();
+    }
+  });
+
   renderHTML(list);
 };
 
@@ -37,10 +91,14 @@ function renderHTML(arrOfObj) {
     <div class="usr_card">
       <img class="user_img" src="${e.picture.large}">
       <p class="usr_name">${e.name.first} ${e.name.last}</p>
+      <span class="">${e.dob.age} yers</span>
       <span class="location">${e.location.state}: ${e.location.city}</span>
       </div>
     `);
   })
 };
 
+// function compareNumbers(a, b) {
+//   return a - b;
+// }
 sortBtn.addEventListener('click', () => header.classList.toggle('open'));
